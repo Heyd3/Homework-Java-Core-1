1ec4c3fea2f1b170a2eb99b274a30389569a37f0
Evgeni <87702959+Heyd3@users.noreply.github.com>
2021-08-05 10:12:09 +0300

    Update Main.java

--- Calculate/Main.java
+++ Calculate/Main.java
@@ -5,9 +5,12 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(2, 2);
-        int b = calc.minus.apply(3,1);
-        int c = calc.devide.apply(a, b);   //ошибка была потому из-за деления на ноль
-
+        int b = calc.minus.apply(1,1);
+        int c = 10;
+        try {
+        c = calc.devide.apply(a, b);   //ошибка была потому из-за деления на ноль
+        } catch (ArithmeticException exception) {System.out.println("Деление на ноль");
+        }
         calc.println.accept(c);
         calc.println.accept(b);
         calc.println.accept(a);
